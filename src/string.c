807fa826298fea3a40f3d9ce76d67cec4b4bfc7a
programming101dev <113087924+programming101dev@users.noreply.github.com>
2025-10-12 20:49:38 -0700

    fix compiler issue

--- src/string.c
+++ src/string.c
@@ -77,14 +77,17 @@ char *p101_strdup(const struct p101_env *env, struct p101_error *err, const char
 
 int p101_strerror_r(const struct p101_env *env, struct p101_error *err, int errnum, char *strerrbuf, size_t buflen)
 {
-    int ret_val = 0;
+    int ret_val;
 
     P101_TRACE(env);
+    ret_val = 0;
     errno = 0;
 
 #if defined(__GLIBC__) && defined(_GNU_SOURCE)
     /* GNU variant: returns char* (may be strerrbuf or static storage). */
-    char *res = strerror_r(errnum, strerrbuf, buflen);
+    char *res;
+
+    res = strerror_r(errnum, strerrbuf, buflen);
 
     if(res == NULL)
     {
@@ -98,6 +101,9 @@ int p101_strerror_r(const struct p101_env *env, struct p101_error *err, int errn
     /* Ensure result ends up in caller buffer for consistent behavior. */
     if(res != strerrbuf)
     {
+        size_t n;
+        size_t copy;
+
         if(buflen == 0)
         {
             ret_val = ERANGE;
@@ -105,8 +111,10 @@ int p101_strerror_r(const struct p101_env *env, struct p101_error *err, int errn
             P101_ERROR_RAISE_ERRNO(err, errno);
             return ret_val;
         }
-        size_t n    = strlen(res);
-        size_t copy = (n < buflen - 1) ? n : (buflen > 0 ? buflen - 1 : 0);
+
+        n    = strlen(res);
+        copy = (n < buflen - 1) ? n : (buflen > 0 ? buflen - 1 : 0);
+
         if(copy > 0)
         {
             memcpy(strerrbuf, res, copy);
@@ -118,6 +126,7 @@ int p101_strerror_r(const struct p101_env *env, struct p101_error *err, int errn
 #else
     /* POSIX variant: returns int (0 on success, error number on failure). */
     ret_val = strerror_r(errnum, strerrbuf, buflen);
+
     if(ret_val != 0)
     {
         /* Some implementations set errno to ret_val; normalize it. */
